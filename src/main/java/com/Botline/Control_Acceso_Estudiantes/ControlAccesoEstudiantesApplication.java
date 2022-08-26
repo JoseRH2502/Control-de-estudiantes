a192b90ea8bee71c66b05d104df90dfec9a22503
JoseRH25 <jlgamer25.ah@gmail.com>
2022-08-26 15:27:25 -0600

    Se agrega el proyecto

---
+++ src/main/java/com/Botline/Control_Acceso_Estudiantes/ControlAccesoEstudiantesApplication.java
@@ -0,0 +1,13 @@
+package com.Botline.Control_Acceso_Estudiantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlAccesoEstudiantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlAccesoEstudiantesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Botline/Control_Acceso_Estudiantes/ControlAccesoEstudiantesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Botline.Control_Acceso_Estudiantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlAccesoEstudiantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
